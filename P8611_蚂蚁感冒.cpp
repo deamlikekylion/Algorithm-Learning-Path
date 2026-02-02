356ea46ec02fca1f500c958b0dffff2e9f7ba207
deamlikekylion <3775956588@qq.com>
2026-02-02 21:20:43 +0800

    Update2026/2/2

---
+++ P8611_蚂蚁感冒.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,x[55],l=0,r=0,sum=0;  cin>>n;
+    for(int i=1;i<=n;i++)  cin>>x[i];
+    for(int i=2;i<=n;i++){
+        if(abs(x[i])<abs(x[1])&&x[i]>0) l++;
+        if(abs(x[i])>abs(x[1])&&x[i]<0) r++;
+    }
+    if(x[1]<0){
+        if(l==0)  sum=1;
+        else  sum=l+r+1;
+    }else{
+        if(r==0)  sum=1;
+        else  sum=l+r+1;
+    }
+    cout<<sum;
+    return 0;
+}
--- 测试代码.cpp
+++ 测试代码.cpp
@@ -1,26 +1,62 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n,k;
-int h[100002];
-int w[100002];
-
-bool check(int x){//判断是否大于等于 k
-	int s=0;
-	for(int i=1; i<=n; i++) s+=(h[i]/x)*(w[i]/x);
-	if(s>=k) return true;
-	else return false;
+long long n,a[500005],b[500005],t[500005],cnt[500005],ans;
+map<long long,int>m;
+long long lowbit(long long x)
+{
+    return x&(-x);
 }
-
-int main(){
-	cin>>n>>k;
-	for(int i=1; i<=n; i++) cin>>h[i]>>w[i];
-	int l=1,r=100002;
-	while(l<r){//二分
-		int mid=(l+r+1)/2;
-		if(check(mid)) l=mid;
-		else r=mid-1;
-	}
-	cout<<l;
-	return 0;
+long long getsum(long long x)
+{
+    long long sum=0;
+    while(x)
+    {
+        sum+=t[x];
+        x-=lowbit(x);
+    }
+    return sum;
+}
+void update(long long x,long long y)
+{
+    while(x<=500000)
+    {
+        t[x]+=y;
+        x+=lowbit(x);
+    }
+}
+int main()
+{
+    scanf("%lld",&n);
+    for (int i = 1; i <= n; i++)
+    {
+        scanf("%lld",&a[i]);
+        b[i]=a[i];
+    }
+    sort(b+1,b+n+1);
+    for(int i=1;i<=n;i++)
+    {
+        m[b[i]]=i;
+    }
+    for(int i=1;i<=n;i++)
+    {
+        a[i]=m[a[i]];
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        update(a[i],1);//数字可以是0，所以要加1
+        cnt[i]+=(i-getsum(a[i])); //统计前面比它大的数字
+    }
+    memset(t,0,sizeof(t));
+    for (int i = n; i >= 1; i--)
+    {
+        update(a[i],1);
+        cnt[i]+=getsum(a[i]-1); //统计后面比它小的数字
+    }
+    for(int i=1;i<=n;i++)
+    {
+    	ans+=(cnt[i]+1)*cnt[i]/2;//算出这个数字的代价
+    }
+    printf("%lld",ans);
+    return 0;
 }
